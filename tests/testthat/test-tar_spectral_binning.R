c856c3139fd1344a7777d74313c43c9865ec5bac
jasenfinch <jsf9@aber.ac.uk>
2022-02-08 13:18:32 +0000

    added unit tests for tar_spectral_binning

---
+++ tests/testthat/test-tar_spectral_binning.R
@@ -0,0 +1,24 @@
+test_that("tar_input_spectral_binning() works", {
+    targets <- tar_spectral_binning(test)
+    
+    expect_identical(class(targets),'list')
+})
+
+test_that("tar_input_spectral_binning() throws an error if an incorrect parameters object is specified", {
+    expect_error(tar_spectral_binning(test,
+                                      parameters = 'wrong'))
+})
+
+test_that("tar_input_spectral_binning() works if `mzML`,`sample_info` and `parameters` arguments are specified directly", {
+    file_paths <- metaboData::filePaths('FIE-HRMS','UrineTechnical',
+                                        ask = FALSE)
+    sample_info <- metaboData::runinfo('FIE-HRMS','UrineTechnical',
+                                       ask = FALSE)
+    
+    targets <- tar_spectral_binning(example,
+                                    mzML = file_paths,
+                                    sample_info = sample_info,
+                                    parameters = binneR::binParameters())
+    
+    expect_identical(class(targets),'list')
+})
